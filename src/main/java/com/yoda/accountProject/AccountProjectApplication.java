0ddc343d5544605c696b1af32d5fc2954f825134
yoda <[aslopeys941@gmail.com]>
2025-06-12 15:52:27 +0900

    chore: h2 DB 설정

---
+++ src/main/java/com/yoda/accountProject/AccountProjectApplication.java
@@ -0,0 +1,13 @@
+package com.yoda.accountProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoda/accountProject/AccountProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoda.accountProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
