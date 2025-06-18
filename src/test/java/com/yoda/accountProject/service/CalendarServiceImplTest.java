a70a46d155a5287333a561754c26083decb45517
yoda <[aslopeys941@gmail.com]>
2025-06-18 20:09:17 +0900

    test: Calendar Test 코드 추가(save)

---
+++ src/test/java/com/yoda/accountProject/service/CalendarServiceImplTest.java
@@ -0,0 +1,60 @@
+package com.yoda.accountProject.service;
+
+
+import com.yoda.accountProject.calendar.domain.Calendar;
+import com.yoda.accountProject.calendar.dto.CalendarRequestDto;
+import com.yoda.accountProject.calendar.dto.CalendarResponseDto;
+import com.yoda.accountProject.calendar.repository.CalendarRepository;
+import com.yoda.accountProject.calendar.service.CalendarServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+@Slf4j
+class CalendarServiceImplTest {
+
+    @Autowired
+    private CalendarRepository calendarRepository;
+    @Autowired
+    private CalendarServiceImpl calendarServiceImpl;
+
+
+
+    @Test
+    @DisplayName("saveCalendar 메서드 테스트1 - Calendar request Dto 를 받아 엔티티에 맵핑하고 저장 후 response Dto를 반환하는 메서드다.")
+    void saveCalendar_method_test1() throws Exception {
+
+        // given
+
+        CalendarRequestDto reqDto = CalendarRequestDto.builder()
+                .date("2025-01-01")
+                .title("테스트 제목")
+                .build();
+
+        //when
+
+        CalendarResponseDto resDto = calendarServiceImpl.saveCalendar(reqDto);
+
+        Calendar entity = calendarRepository.findById(resDto.getId())
+                .orElseThrow(() -> new Exception());
+
+        //then
+        assertThat(entity.getId()).isEqualTo(resDto.getId());
+
+        log.info("entity.getId() = {}", entity.getId());
+        log.info("resDto.getId() = {}", resDto.getId());
+
+
+    }
+
+
+
+}
